0c7a0738afde850bf309839747fed1cd9135b062
vuthanhbayit <37856246+vuthanhbayit@users.noreply.github.com>
2018-10-06 17:44:44 +0700

    1
    1

--- Prj HTML/js/ShrinkNavOnScroll.js
+++ Prj HTML/js/ShrinkNavOnScroll.js
@@ -1,10 +1,12 @@
 $(window).scroll(function() {
   if ($(document).scrollTop() > 10) {
     $('.icon-search').addClass('icon-search-scroll');
+    $('.icon-cart').addClass('icon-cart-scroll');
     $('.navbar').addClass('bg-dark navbar-dark affix ');
    
   } else {
   	$('.icon-search').removeClass('icon-search-scroll');
+  	$('.icon-cart').removeClass('icon-cart-scroll');
     $('.navbar').removeClass('affix bg-dark navbar-dark'); 
   }
 });
