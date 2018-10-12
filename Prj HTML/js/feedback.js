8a8164b0b809c3ef44e02e144d69625b59c1d457
hoangvanque <hoangvanque89@gmail.com>
2018-10-12 11:51:47 +0700

    1

--- Prj HTML/js/feedback.js
+++ Prj HTML/js/feedback.js
@@ -441,8 +441,8 @@ function feedback()
     } 
    
    
-    return true; 
-// alert("Thanks You")
+    return alert("Thanks for the feedback");
+
 }
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
