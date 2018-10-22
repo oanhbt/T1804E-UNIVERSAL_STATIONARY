f8acf4f119f1ea4ba385502b4bb1f8f8742ed884
vuthanhbayit <37856246+vuthanhbayit@users.noreply.github.com>
2018-10-22 23:03:23 +0700

    1
    1

--- Prj HTML/js/validate-form.js
+++ Prj HTML/js/validate-form.js
@@ -1,9 +1,8 @@
-
-function GEEKFORGEEKS()                                    
+function signup()                                    
 { 
-    var name = document.forms["RegForm"]["Name"];               
-    var email = document.forms["RegForm"]["EMail"];    
-    var password = document.forms["RegForm"]["Password"];   
+    var name = document.forms["form1"]["Name"];               
+    var email = document.forms["form1"]["EMail"];     
+    var password = document.forms["form1"]["Password"];  
    
     if (name.value == "")                                  
     { 
@@ -32,30 +31,48 @@ function GEEKFORGEEKS()
         email.focus(); 
         return false; 
     } 
-	
-	if (!validateEmail(email.value ))                 
+    
+    if (!validateEmail(email.value ))                 
     { 
         window.alert("Please enter a valid e-mail address."); 
         email.focus(); 
         return false; 
     } 
-   
     if (password.value == "")                        
     { 
         window.alert("Please enter your password"); 
         password.focus(); 
         return flase; 
     } 
-   
-    if (what.selectedIndex < 1)                  
-    { 
-        alert("Please enter your course."); 
-        what.focus(); 
-        return false; 
-    } 
-   
+
+    alert("Dang ky thanh cong");
     return true; 
 }
+
+// function signin()                                    
+// { 
+//     var name = document.forms["RegForm1"]["signinName"];
+//     var password = document.forms["RegForm1"]["signinPass"];
+   
+//     if (name.value == "")                                  
+//     { 
+//         window.alert("Please enter your name."); 
+//         name.focus(); 
+//         return false; 
+//     } 
+   
+   
+//     if (password.value == "")                        
+//     { 
+//         window.alert("Please enter your password"); 
+//         password.focus(); 
+//         return flase; 
+//     } 
+   
+//     alert("Dang nhap thanh cong");
+//     return true; 
+// }
+
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
